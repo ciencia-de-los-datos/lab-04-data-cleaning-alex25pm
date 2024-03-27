6f6845c0ead0902c3672c5bd210235d7cfc4e322
Alexander Peña <118406605+alex25pm@users.noreply.github.com>
2024-03-26 21:20:58 -0500

    intento 1

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,16 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+    df.dropna(inplace=True)
 
-    #
-    # Inserte su código aquí
-    #
+    
+    df = df.apply(lambda x: x.astype(str).str.replace("_","-").str.replace("-"," ").str.replace("$","").str.replace(",",""))
+    df = df.apply(lambda x: x.str.lower())
+
+    df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"],format='mixed', dayfirst = True)
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+
+    df.drop_duplicates(inplace=True)
 
     return df
